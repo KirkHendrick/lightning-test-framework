2e087f8b91e2219f349861680c4bc679b9beb011
Kirk Hendrick <hendrick.kirk@gmail.com>
2016-11-11 16:18:39 -0500

    updated gulpfile and MockComponent from dev branch

--- MockComponent.js
+++ MockComponent.js
@@ -2,71 +2,162 @@
 
     'use strict';
 
-    var MockComponent = function (attributes, elements, registeredEvents, eventHandlers, controller) {
-        return new MockComponent.init(attributes, elements, registeredEvents, eventHandlers, controller);
-    };
-
-    MockComponent.prototype = {
-        get: function (attributeName) {
-            if (attributeName[0] === 'c') {
-                var method = this.getReference(attributeName);
-                method['setCallback'] = function (context, callback) {
-                    method['callback'] = callback;
-                };
-                return method;
+    var MockComponent = function (attributes, elements, registeredEvents, eventHandlers, apexController, controller, helper) {
+        if (attributes === undefined) {
+            return new MockComponent.init();
+        }
+        else {
+            if (!Array.isArray(attributes)) {
+                const options = attributes;
+                attributes = options.attributes || [];
+                elements = options.elements || [];
+                registeredEvents = options.registeredEvents || [];
+                eventHandlers = options.eventHandlers || [];
+                apexController = options.apexController || {};
+                controller = options.controller || {};
+                helper = options.helper || {};
             }
 
-            return this.attributes.find(function (obj) {
-                return obj.name === attributeName.slice(2);
-            }).value;
-        },
-
-        set: function (attributeName, newValue) {
-            var attribute = this.attributes.find(function (obj) {
-                return obj.name === attributeName.slice(2);
-            });
-            attribute.value = newValue;
-        },
-
-        find: function (auraId) {
-            return this.elements.find(function (obj) {
-                return obj.auraId === auraId;
-            });
-        },
-
-        getEvent: function (eventName) {
-            return this.registeredEvents.find(function (obj) {
-                return obj.name === eventName;
-            });
-        },
-
-        getReference: function (methodName) {
-            return this.controller[methodName.slice(2)];
+            return new MockComponent.init(attributes, elements, registeredEvents, eventHandlers, apexController, controller, helper);
         }
     };
 
-    MockComponent.init = function (attributes, elements, registeredEvents, eventHandlers, controller) {
+    MockComponent.prototype = {
+        get: function (input) {
+            const self = this;
+
+            if (input[0] === 'c') {
+                return getControllerAction(input);
+            }
+            else if (input[0] === 'v') {
+                return getAttribute(input);
+            }
+
+            function getControllerAction(input) {
+                var action = self.getReference(input);
+
+                try {
+                    action.setParams = function (params) {
+                        action.params = Object.keys(params)
+                            .map(function (param) {
+                                if (params.hasOwnProperty(param)) {
+                                    return params[param];
+                                }
+                            });
+                    };
+
+                    action.setCallback = function (context, callback) {
+                        action.callback = callback;
+                    };
+                }
+                catch (e) {
+                    throw new ReferenceError(
+                        'action "' + input.slice(2) + '" is not defined'
+                    );
+                }
+
+                return action;
+            }
+
+            function getAttribute(input) {
+                try {
+                    return self.attributes.find(function (obj) {
+                        return obj.name === input.slice(2);
+                    }).value;
+                }
+                catch (e) {
+                    throw new ReferenceError('attribute "' + input.slice(2) + '" is not defined');
+                }
+            }
+        },
+
+        set: function (attributeName, newValue) {
+            this.attributes.find(function (obj) {
+                return obj.name === attributeName.slice(2);
+            }).value = newValue;
+        },
+
+        find: function (auraId) {
+            const element = this.elements.find(function (obj) {
+                return obj.auraId === auraId;
+            });
+
+            if (element === undefined) {
+                throw new ReferenceError(
+                    'element "' + auraId + '" is not defined'
+                );
+            }
+
+            return element;
+        },
+
+        getEvent: function (eventName) {
+            const event = this.registeredEvents.find(function (obj) {
+                return obj.name === eventName;
+            });
+
+            if (event === undefined) {
+                throw new ReferenceError(
+                    'event "' + eventName + '" is not registered'
+                );
+            }
+
+            return event;
+        },
+
+        getReference: function (actionName) {
+            return this.apexController[actionName.slice(2)];
+        },
+
+        isValid: function () {
+            return true;
+        }
+    };
+
+    MockComponent.init = function (attributes, elements, registeredEvents, eventHandlers, apexController, controller, helper) {
         this.attributes = attributes || [];
         this.elements = elements || [];
         this.registeredEvents = registeredEvents || [];
         this.eventHandlers = eventHandlers || [];
+        this.apexController = apexController || {};
         this.controller = controller || {};
+        this.helper = helper || {};
 
         associateEventHandlers.call(this);
     };
 
     function associateEventHandlers() {
-        this.registeredEvents.forEach(function (registeredEvent) {
-            const associatedHandlers = this.eventHandlers.filter(function (handler) {
+        const self = this;
+        self.registeredEvents.forEach(function (registeredEvent) {
+            const associatedHandlers = self.eventHandlers.filter(function (handler) {
                 return handler.name === registeredEvent.name;
             });
 
-            registeredEvent['fire'] = function () {
+            registeredEvent.setParams = function (params) {
+                registeredEvent.params = params;
+                return registeredEvent;
+            };
+
+            registeredEvent.fire = function () {
                 associatedHandlers.forEach(function (handler) {
-                    handler.action();
+                    try {
+                        self.controller[handler.action.slice(2)](self, {
+                                getParam: function (param) {
+                                    return registeredEvent.params[param];
+                                },
+                                stopPropagation: function () {
+                                }
+                            },
+                            self.helper);
+                    }
+                    catch (e) {
+                        throw new ReferenceError(
+                            'controller method "' + handler.action.slice(2) + '" does not exist'
+                        );
+                    }
                 });
             };
-        }, this);
+        });
     }
 
     MockComponent.init.prototype = MockComponent.prototype;
@@ -74,4 +165,3 @@
     exports.MockComponent = MockComponent;
 
 })(this);
-
--- gulpfile.js
+++ gulpfile.js
@@ -3,25 +3,42 @@ const gulp = require('gulp'),
 	settings = require('./settings');
 
 gulp.task('convert', function () {
+	validateSettings();
 	settings.componentBundles.forEach(function (bundleName) {
 		convertController(bundleName);
 		convertHelper(bundleName);
 	});
 });
 
+function validateSettings() {
+	if(settings.ltfDirectory.slice(-1) === '/') {
+		settings.ltfDirectory = settings.ltfDirectory.slice(0, -1);
+	}
+
+	if(settings.buildDirectory.slice(-1) === '/') {
+		settings.buildDirectory = settings.buildDirectory.slice(0, -1);
+	}
+
+	if(settings.auraDirectory.slice(-1) === '/') {
+		settings.auraDirectory = settings.auraDirectory.slice(0, -1);
+	}
+}
+
 function parseLightning(content, type) {
-	return "var Mock$A = require('./../Mock$A').Mock$A," +
+	return "var Mock$A = require('" + settings.ltfDirectory + "/Mock$A').Mock$A," +
 		type + " = (function($A) { " +
 		"'use strict'; " +
-		"return " +
+		"var publicApi = " +
 
 		content
 			.substr(content.indexOf('{'))
 			.trim()
 			.slice(0, -1)
+			.replace(/(\/\*([\s\S]*?)\*\/)|(\/\/(.*)$)/gm, '')
 			.replace(/(\r\n|\n|\r)/gm, "") +
 
-		" })(Mock$A()); " +
+		"; return publicApi; " +
+		"})(Mock$A()); " +
 		"exports." + type + " = " + type + "; ";
 }
 
